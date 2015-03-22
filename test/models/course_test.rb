43ec812226d3e92e3d7db7beb77e453eedf4fb6f
Harvey Bridges <hdbridges11@gmail.com>
2015-03-22 17:08:52 +0000

    test error on validate

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Course Create Validate' do
+  	user = User.create :email => 'test@gmail.com', :password => 'password'
+ 	course  = user.courses.build
+ 	assert_not course.valid?
+ 	assert_equal [:title, :description, :cost], course.error.keys
+  end
 end
