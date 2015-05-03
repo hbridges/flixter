4f40a30283b9a7930e4c18871eb76916364f3e61
Harvey Bridges <hdbridges11@gmail.com>
2015-05-03 17:32:59 +0000

    added instructor ability to reorder lessons with js

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,11 @@
 class CoursesController < ApplicationController
 	def index
 		@courses = Course.all
+		respond_to do |format|
+  			format.html
+ 		 	format.json { render json: @courses }
+		end
+
 	end
 
 	def show
